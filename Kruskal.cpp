0feba92847bbbb8cc61516e24f385fc38e37159c
varshavkumar98765 <varshav.ai23@bmsce.ac.in>
2025-04-15 10:29:37 +0530

    Update Kruskal.cpp

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -98,54 +98,30 @@ int main() {
   int i, j, total_cost;
 
   n = 6;
+  
+  for(i=0;i<n;i++)for(j=0;j<n;j++)Graph[i][j]=0;
 
-  Graph[0][0] = 0;
   Graph[0][1] = 4;
   Graph[0][2] = 4;
-  Graph[0][3] = 0;
-  Graph[0][4] = 0;
-  Graph[0][5] = 0;
-  Graph[0][6] = 0;
 
   Graph[1][0] = 4;
-  Graph[1][1] = 0;
   Graph[1][2] = 2;
-  Graph[1][3] = 0;
-  Graph[1][4] = 0;
-  Graph[1][5] = 0;
-  Graph[1][6] = 0;
 
   Graph[2][0] = 4;
   Graph[2][1] = 2;
-  Graph[2][2] = 0;
+
   Graph[2][3] = 3;
   Graph[2][4] = 4;
-  Graph[2][5] = 0;
-  Graph[2][6] = 0;
 
-  Graph[3][0] = 0;
-  Graph[3][1] = 0;
   Graph[3][2] = 3;
-  Graph[3][3] = 0;
   Graph[3][4] = 3;
-  Graph[3][5] = 0;
-  Graph[3][6] = 0;
 
-  Graph[4][0] = 0;
-  Graph[4][1] = 0;
   Graph[4][2] = 4;
   Graph[4][3] = 3;
-  Graph[4][4] = 0;
-  Graph[4][5] = 0;
-  Graph[4][6] = 0;
 
-  Graph[5][0] = 0;
-  Graph[5][1] = 0;
   Graph[5][2] = 2;
-  Graph[5][3] = 0;
   Graph[5][4] = 3;
-  Graph[5][5] = 0;
-  Graph[5][6] = 0;
+
 
   kruskalAlgo();
   print();
