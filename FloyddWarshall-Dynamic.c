5c876ddd1d0a834cabf1173875820377eba4553e
varshavkumar98765 <varshav.ai23@bmsce.ac.in>
2025-04-30 11:43:17 +0530

    Add files via upload

---
+++ FloyddWarshall-Dynamic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAX 3
+
+int b[MAX][MAX]={0};
+void floyds() {
+   int i, j, k;
+   for (k = 0; k < 3; k++) {
+      for (i = 0; i < 3; i++) {
+         for (j = 0; j < 3; j++) {
+            if ((b[i][k] * b[k][j] != 0) && (i != j)) {
+               if ((b[i][k] + b[k][j] < b[i][j]) || (b[i][j] == 0)) {
+                  b[i][j] = b[i][k] + b[k][j];
+               }
+            }
+         }
+      }
+   }
+   for (i = 0; i < 3; i++) {
+      printf("Minimum Cost With Respect to Node: %d\n", i);
+      for (j = 0; j < 3; j++) {
+         printf("%d\t", b[i][j]);
+      }
+   }
+}
+
+int main() {
+   b[0][1] = 10;
+   b[1][2] = 15;
+   b[2][0] = 12;
+   floyds(b);
+   return 0;
+}
